c77a5f366ceef577a390c6927b86fb4c83a8ff3f
yswang123 <ys.wang0815@gmail.com>
2021-11-18 23:21:41 -0600

    Update description

--- sumyw_develop.R
+++ sumyw_develop.R
@@ -1,4 +1,9 @@
 library(devtools)
+library(desc)
 create_package("../sumyw")
 
 use_git()
+use_github()
+desc_add_author(given = "Yushu", family = "Wang", email = "yuswang@mcw.edu ",
+                role = c("aut","cre"))
+use_gpl3_license()
